dbde564dfc9b35a4f80efedfeec6f6b416729143
Connor Floyd <cwfloyd@email.uscb.edu>
2024-10-13 22:34:08 -0400

    assignment 8.1 finished

--- assignment_8_1.sql
+++ assignment_8_1.sql
@@ -6,28 +6,28 @@
 */
 
 -- Exercise 1
---SELECT *
---INTO VendorCopy
---FROM Vendors
+SELECT *
+INTO VendorCopy
+FROM Vendors
 
---SELECT *
---INTO InvoiceCopy
---FROM Invoices
+SELECT *
+INTO InvoiceCopy
+FROM Invoices
 
 -- Exercise 2
---INSERT
---INTO InvoiceCopy (	
---	VendorID,		InvoiceTotal, 
---	TermsID,		InvoiceNumber,
---	PaymentTotal,	InvoiceDueDate,
---	InvoiceDate,	CreditTotal,
---	PaymentDate)
---VALUES (
---	32,				434.58,
---	2,				'AX-014-027',
---	0.00,			'2023-05-08',
---	'2023-04-21',	0.00,
---	NULL)
+INSERT
+INTO InvoiceCopy (	
+	VendorID,		InvoiceTotal, 
+	TermsID,		InvoiceNumber,
+	PaymentTotal,	InvoiceDueDate,
+	InvoiceDate,	CreditTotal,
+	PaymentDate)
+VALUES (
+	32,				434.58,
+	2,				'AX-014-027',
+	0.00,			'2023-05-08',
+	'2023-04-21',	0.00,
+	NULL)
 
 -- Exercise 3
 INSERT INTO VendorCopy
@@ -42,4 +42,35 @@ WHERE DefaultAccountNo = 400
 
 -- Exercise 5
 UPDATE InvoiceCopy
-SET PaymentDate = GETDATE(), PaymentTotal = InvoiceTotal
+SET PaymentDate = GETDATE(), PaymentTotal = (InvoiceTotal - CreditTotal - PaymentTotal)
+WHERE (InvoiceTotal - CreditTotal - PaymentTotal) > 0
+
+-- Exercise 6
+UPDATE InvoiceCopy
+SET TermsID = 2
+WHERE VendorID IN (	
+	SELECT *
+	FROM VendorCopy
+	WHERE DefaultTermsID = 2
+)
+
+-- Exercise 7
+UPDATE InvoiceCopy
+SET TermsID = 2
+FROM InvoiceCopy
+	INNER JOIN VendorCopy ON InvoiceCopy.VendorID = VendorCopy.VendorID
+WHERE DefaultTermsID = 2
+
+-- Exercise 8
+DELETE
+FROM VendorCopy
+WHERE VendorState = 'MN'
+
+-- Exercise 9
+DELETE FROM VendorCopy
+WHERE VendorState NOT IN (	
+	SELECT DISTINCT VendorState
+	FROM VendorCopy
+		JOIN InvoiceCopy ON VendorCopy.VendorID = InvoiceCopy.VendorID
+)
+
